2898f11331537410c272486c1b23bfabb421c084
sagar <sagar@192.168.0.5>
2020-05-01 21:09:26 -0400

    spellcheckResult file updated

--- spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/util/SpellCheckResults.java
+++ spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/util/SpellCheckResults.java
@@ -1,31 +1,74 @@
 package spellCheckerVisitorPlay.util;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class SpellCheckResults implements Results {
+	private String spellCheckOutputFilename;
+	private File outputFile;
+	private FileWriter outputFileWriter;
+	private static List<List<String>> result;
 
-	public SpellCheckResults(String itemOutputFileIn) {
-		// TODO Auto-generated constructor stub
+	public SpellCheckResults(String spellCheckOutputFilenameIn) {
+		spellCheckOutputFilename=spellCheckOutputFilenameIn;
+		try {
+			outputFile = new File(spellCheckOutputFilename);
+			outputFileWriter = new FileWriter(spellCheckOutputFilename,false);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 
 	@Override
 	public void writeToFile() {
-		// TODO Auto-generated method stub
+		try {
+
+			if (!Files.exists(Paths.get(spellCheckOutputFilename))) {
+				
+				outputFile.createNewFile();
+
+				
+				outputFileWriter.write(result + "\n");
+				//outputFileWriter.flush();
 		
+			} else {
+				//outputFileWriter = new FileWriter(itemOutputFile,true);
+					for(List<String> res: result) {
+					outputFileWriter.write(res.toString() + "\n");
+					outputFileWriter.flush();
+					}
+					
+			}
+			
+			//close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
+		try {
+			//outputFileWriter.flush();
+			outputFileWriter.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 
 	@Override
 	public void setResult(List<String> topK) {
-		// TODO Auto-generated method stub
+		result.add(topK);
+
 		
 	}
-
 }
