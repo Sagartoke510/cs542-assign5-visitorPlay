3c95cbaedb58711ef52bf615efcdeee2233b3fa1
sagar <sagar@192.168.0.5>
2020-05-02 16:43:48 -0400

    Spellcheck logic updated with result file write logic

--- spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/util/SpellCheckResults.java
+++ spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/util/SpellCheckResults.java
@@ -5,48 +5,40 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class SpellCheckResults implements Results {
 	private String spellCheckOutputFilename;
 	private File outputFile;
 	private FileWriter outputFileWriter;
-	private static List<List<String>> result;
+	private static String result = "";
 
 	public SpellCheckResults(String spellCheckOutputFilenameIn) {
-		spellCheckOutputFilename=spellCheckOutputFilenameIn;
+		spellCheckOutputFilename = spellCheckOutputFilenameIn;
 		try {
 			outputFile = new File(spellCheckOutputFilename);
-			outputFileWriter = new FileWriter(spellCheckOutputFilename,false);
+			outputFileWriter = new FileWriter(spellCheckOutputFilename, false);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 
 	@Override
 	public void writeToFile() {
 		try {
 
 			if (!Files.exists(Paths.get(spellCheckOutputFilename))) {
-				
+
 				outputFile.createNewFile();
 
-				
 				outputFileWriter.write(result + "\n");
-				//outputFileWriter.flush();
-		
+
 			} else {
-				//outputFileWriter = new FileWriter(itemOutputFile,true);
-					for(List<String> res: result) {
-					outputFileWriter.write(res.toString() + "\n");
-					outputFileWriter.flush();
-					}
-					
+
+				outputFileWriter.write(result.toString());
+				outputFileWriter.flush();
+
 			}
-			
-			//close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -55,20 +47,16 @@ public class SpellCheckResults implements Results {
 	@Override
 	public void close() {
 		try {
-			//outputFileWriter.flush();
 			outputFileWriter.close();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-
 	@Override
-	public void setResult(List<String> topK) {
-		result.add(topK);
-
-		
+	public void setResult(String resultIn) {
+		result = result + resultIn + "\n";
 	}
 }
--- spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/visitor/SpellCheckAnalyzer.java
+++ spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/visitor/SpellCheckAnalyzer.java
@@ -1,34 +1,86 @@
 package spellCheckerVisitorPlay.visitor;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import spellCheckerVisitorPlay.util.FileProcessor;
 import spellCheckerVisitorPlay.util.Results;
 
 public class SpellCheckAnalyzer implements Visitor {
 
 	private String acceptableWordsFilename;
 	private Results spellCheckResults;
-	FileProcessor fp;
+	private static Boolean fileRead = false;
+	private static List<String> acceptableWords = new ArrayList<String>();
 
-	public SpellCheckAnalyzer(String acceptableWordsFilenameIn, Results spellCheckResultsIn) throws IOException {
+	public SpellCheckAnalyzer(String acceptableWordsFilenameIn, Results spellCheckResultsIn) {
 		acceptableWordsFilename = acceptableWordsFilenameIn;
 		spellCheckResults = spellCheckResultsIn;
-		List<String> acceptableWords = new ArrayList<String>();
-		while (fp.getLine() != null) {
-			String lineFromFile = fp.poll();
-			acceptableWords.add(lineFromFile);
-
-		}
 	}
 
 	@Override
-	public void visit(MyElement myElement) {
+	public void visit(MyElement myElement) throws IOException {
 		String[] wordList = myElement.getSentence().split("\\s");
-		
+		if (!fileRead) {
+			readAccetableFile();
+		}
 
+		for (String word : wordList) {
+			if (!acceptableWords.contains(word)) {
+
+				char[] words = word.toLowerCase().toCharArray();
+				List<String> possibleAcceptableWords = new ArrayList<String>();
+				Integer noMatch = 0;
+				int match = 0;
+				int flag = 0;
+				for (String wordAccept : acceptableWords) {
+					char[] acceptableWordsChar = wordAccept.toLowerCase().toCharArray();
+					Character[] matchWord = new Character[wordAccept.length()];
+					if (acceptableWordsChar.length != words.length) {
+						noMatch++;
+					} else {
+						for (int i = 0; i < acceptableWordsChar.length; i++) {
+							if (acceptableWordsChar[i] == words[i]) {
+								matchWord[i] = words[i];
+								match++;
+							}
+							flag = matchWord.length;
+
+						}
+						for (int j = 0; j < matchWord.length; j++) {
+							if (matchWord[j] == null) {
+								flag--;
+							}
+						}
+
+						if (flag == acceptableWordsChar.length - 1) {
+							possibleAcceptableWords.add(wordAccept);
+						}
+
+					}
+
+				}
+
+				if (!possibleAcceptableWords.isEmpty())
+					spellCheckResults.setResult(word + "::" + possibleAcceptableWords);
+
+			}
+
+		}
+
+	}
+
+	protected void readAccetableFile() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(new File(acceptableWordsFilename)));
+		String line = "";
+		while ((line = reader.readLine()) != null) {
+			acceptableWords.add(line);
+		}
+		fileRead = true;
+		reader.close();
 	}
 
 }
