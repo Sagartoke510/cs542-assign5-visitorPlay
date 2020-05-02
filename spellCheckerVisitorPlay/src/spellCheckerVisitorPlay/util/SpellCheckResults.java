abb77b7f6cf43b05fa03c3558cb9254080aa06be
sagar <sagar@192.168.0.5>
2020-05-01 21:02:13 -0400

    spellcheck files initial commit

---
+++ spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/util/SpellCheckResults.java
@@ -0,0 +1,31 @@
+package spellCheckerVisitorPlay.util;
+
+import java.util.List;
+
+public class SpellCheckResults implements Results {
+
+	public SpellCheckResults(String itemOutputFileIn) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public void writeToFile() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setResult(List<String> topK) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ spellCheckerVisitorPlay/src/spellCheckerVisitorPlay/visitor/SpellCheckAnalyzer.java
@@ -0,0 +1,17 @@
+package spellCheckerVisitorPlay.visitor;
+
+import spellCheckerVisitorPlay.util.Results;
+
+public class SpellCheckAnalyzer implements Visitor {
+
+	public SpellCheckAnalyzer(String acceptableWordsFilename, Results spellCheckResults) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void visit(MyElement myElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
